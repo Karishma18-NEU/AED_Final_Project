cb7e5af3276b03a52681031bbd7ef8f67bc38312
Karishma Murde <murde.k@husky.neu.edu>
2020-04-08 00:55:43 -0400

    Adding Business java classes

---
+++ MediCare/src/Business/Enterprise/Enterprise.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+
+/**
+ *
+ * @author Karishma Murde
+ */
+public abstract class Enterprise extends Organization {
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+    
+    public Enterprise(String name, EnterpriseType enterpriseType) {
+        super(name);
+        this.enterpriseType = enterpriseType;
+        organizationDirectory = new OrganizationDirectory();
+    }
+    
+    public enum EnterpriseType{
+        Trust("Trust"),
+        OldAgeHome("Old Age Home");
+        private String value;
+
+        private EnterpriseType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+        
+        @Override
+        public String toString() {
+            return value;
+        }  
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- MediCare/src/Business/Role/SystemAdminRole.java
+++ MediCare/src/Business/Role/SystemAdminRole.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package Business.Role;
-
+import Business.DB4OUtil.DB4OUtil;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
 /**
  *
  * @author shali
  */
-public class SystemAdminRole {
-    
+public class SystemAdminRole extends Role{
+    public SystemAdminRole(){
+        super(Role.RoleType.System.getValue());
+    }
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, EcoSystem business, DB4OUtil dB4OUtil, UserAccount userAccount, Organization organization, Enterprise enterprise) {
+       return new SystemAdminHomeJPanel(userProcessContainer, business, dB4OUtil, userAccount);
+    }
 }
