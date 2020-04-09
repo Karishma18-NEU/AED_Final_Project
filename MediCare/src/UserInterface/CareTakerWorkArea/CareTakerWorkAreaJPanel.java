2da5796d1cf8b80315b7d5ef3dc3453e78fe8ec6
Vardhana-aed <bhatt.va@husky.neu.edu>
2020-04-09 14:49:47 -0400

    CareTakerWorkArea changes

--- MediCare/src/UserInterface/CareTakerWorkArea/CareTakerWorkAreaJPanel.java
+++ MediCare/src/UserInterface/CareTakerWorkArea/CareTakerWorkAreaJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CareTakerWorkArea;
+package UserInterface.CareTakerWorkArea;
 
 import UserInterface.VolunteerWorkArea.*;
 import Business.EcoSystem;
